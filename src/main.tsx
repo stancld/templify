89474a6533bdfe6ade29898d2836a79ea910c03d
stancld <daniel.stancl@gmail.com>
2026-01-11 20:57:46 +0100

    Install web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.tsx'
 import './index.css'
@@ -7,6 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
     <SpeedInsights />
   </React.StrictMode>,
 )
